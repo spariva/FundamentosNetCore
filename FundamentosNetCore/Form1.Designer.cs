25adbd5a70f849a3bb69e56d86062940e963ed08
Maki Spariva <makspariva@gmail.com>
2024-12-10 12:52:42 +0100

    Add project files.

---
+++ FundamentosNetCore/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace FundamentosNetCore
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAccept = new Button();
+            btnCancel = new Button();
+            txtName = new TextBox();
+            SuspendLayout();
+            // 
+            // btnAccept
+            // 
+            btnAccept.Location = new Point(235, 363);
+            btnAccept.Name = "btnAccept";
+            btnAccept.Size = new Size(75, 23);
+            btnAccept.TabIndex = 0;
+            btnAccept.Text = "button1";
+            btnAccept.UseVisualStyleBackColor = true;
+            btnAccept.Click += btnAccept_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(518, 363);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(75, 23);
+            btnCancel.TabIndex = 1;
+            btnCancel.Text = "btnCancel";
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(274, 70);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(221, 23);
+            txtName.TabIndex = 2;
+            txtName.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtName);
+            Controls.Add(btnCancel);
+            Controls.Add(btnAccept);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAccept;
+        private Button btnCancel;
+        private TextBox txtName;
+    }
+}
---
+++ FundamentosNetCore/Form1.cs
@@ -0,0 +1,24 @@
+namespace FundamentosNetCore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.SuspendLayout();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            this.txtName.Location = new Point(100, 100);
+            this.txtName.Text = "Hello World";
+            this.txtName.Width = 200;
+            this.txtName.TextAlign = HorizontalAlignment.Center;
+        }
+    }
+}
